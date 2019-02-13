8b39ab40a64836a010cbd34836908c4ed20b3a79
sangwon <junothyme@gmail.com>
2019-02-13 11:27:29 +0900

    Uploading and Creating Videos

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ mongoose.connect(process.env.MONGO_URL, {
 const db = mongoose.connection;
 
 const handleOpen = () => console.log("✅  Connected to DB");
-const handleError = err => console.log(`❌ Error on DB Connection:${error}`);
+const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
 
 db.once("open", handleOpen);
 db.on("error", handleError);
