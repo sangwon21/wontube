d844105173d154e2af79339b98e46b9d6028d7b5
Sangwon <junothyme@gmail.com>
2019-02-16 15:23:43 +0900

    Github Log in

--- passport.js
+++ passport.js
@@ -1,7 +1,19 @@
 import passport from "passport";
+import GithubStrategy from "passport-github";
 import User from "./models/User";
 
 passport.use(User.createStrategy());
+
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: process.env.GH_ID,
+      clientSecret: process.env.GH_SECRET,
+      callbackURL: "http://localhost:4000/auth/github/callback"
+    },
+    githubLoginCallback
+  )
+);
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
