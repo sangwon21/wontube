a75de072ed67286910e9d639771b10d66167ffd5
Sangwon <junothyme@gmail.com>
2019-02-23 11:09:51 +0900

    Change Password

--- controllers/userController.js
+++ controllers/userController.js
@@ -95,22 +95,41 @@ export const userDetail = async (req, res) => {
 export const getEditProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "Edit Profile" });
 
-export const postEditProfile = async (req, res) =>{
-  const{
+export const postEditProfile = async (req, res) => {
+  const {
     body: { name, email },
     file
   } = req;
-  try{
+  try {
     await User.findByIdAndUpdate(req.user.id, {
       name,
       email,
       avatarUrl: file ? file.path : req.user.avatarUrl
-    })
+    });
     res.redirect(routes.me);
-  }catch(error){
-    res.render("editProfile", {pageTitle: "Edit Profile"})
+  } catch (error) {
+    res.render("editProfile", { pageTitle: "Edit Profile" });
   }
-}
+};
 
-export const changePassword = (req, res) =>
+export const getChangePassword = (req, res) =>
   res.render("changePassword", { pageTitle: "Change Password" });
+
+export const postChangePassword = async (req, res) => {
+  const {
+    body: { oldPassword, newPassword, newPassword1 }
+  } = req;
+
+  try {
+    if (newPassword !== newPassword1) {
+      res.status(400);
+      res.redirect(`users${routes.changePassword}`);
+      return;
+    }
+    await req.user.changePassword(oldPassword, newPassword);
+    res.redirect(routes.me);
+  } catch (error) {
+    res.status(400);
+    res.redirect(`users${routes.changePassword}`);
+  }
+};
