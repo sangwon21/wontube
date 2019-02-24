f60e2d46cc64eac6f3ffdd16a2d6b4f2be5bb5f8
Sangwon <junothyme@gmail.com>
2019-02-24 18:54:12 +0900

    Recording Video 2

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -1,17 +1,33 @@
 const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
+
 let streamObject;
+let videoRecorder;
 
-const handleVideoData = event =>{
-    console.log(event);
-}
+const handleVideoData = event => {
+  const { data: videoFile } = event;
+  console.log(videoFile);
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
+};
 
-const startRecording = () =>{
-    const videoRecorder  = new MediaRecorder(streamObject);
-    videoRecorder.start();
-    videoRecorder.addEventListener('dataavaliable', handleVideoData);
-}
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start recording";
+};
+
+const startRecording = () => {
+  videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+  recordBtn.addEventListener("click", stopRecording);
+};
 
 const getVideo = async () => {
   try {
@@ -27,9 +43,8 @@ const getVideo = async () => {
     startRecording();
   } catch (error) {
     recordBtn.innerHTML = "Cannot Record!";
-}finally{
+  } finally {
     recordBtn.removeEventListener("click", getVideo);
-
   }
 };
 
